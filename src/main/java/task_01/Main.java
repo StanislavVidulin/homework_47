827def6d8ee9ccab014f8604b65d4fd38d8c5c87
StanislavVidulin <stas_vidulin@icloud.com>
2025-01-29 14:22:10 +0100

    Init commit

---
+++ src/main/java/task_01/Main.java
@@ -0,0 +1,11 @@
+package task_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        int result = calculator.calculateTwoNumbers(5,4);
+        System.out.println("Result is - " + result);
+    }
+}
